04ab400021600350e23a222d5668df6119a9c35f
Lucas Lima <lucas_rj1@hotmail.com>
2024-03-10 19:29:56 -0300

    style: Alteração do ícone para um foguete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -78,8 +80,8 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        child: const Icon(FontAwesomeIcons.rocket),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
